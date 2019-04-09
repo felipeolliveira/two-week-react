19cef7bd12f1004277078751cd269cf2bdb339c3
Felipe Oliveira <flpoliveira42@gmail.com>
2019-04-09 21:02:47 +0000

    Criado o component SquareButton

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React from "react";
+import SquareButton from "./components/SquareButton"/
 
 const App = () => (
   <h1>15 dias de react</h1>
+  <SquareButton />
 );
 
 export default App;
---
+++ app/src/components/SquareButton/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SquareButton = () => (
+  <button></button>;
+)
+
+export default SquareButton;

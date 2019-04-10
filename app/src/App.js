12f4e9f422d397ebbffa94db38ce0ac3e430417c
Felipe Oliveira <flpoliveira42@gmail.com>
2019-04-10 01:34:58 +0000

    Dia 01 - Desafio Básico

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+
 import SquareButton from "./components/SquareButton";
+import BoardSquare from "./components/BoardSquare"
 
 const App = () => (
+  <>
+  <BoardSquare />
   <SquareButton />
+  </>
 );
 
 export default App;
---
+++ app/src/components/BoardSquare/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./style.css"
+
+const BoardSquare = () => (
+  <div className="board-square"></div>
+)
+
+export default BoardSquare;
